5ab09fc4bb30ba685e76723e3855dca4824f0eff
Jussi Vesala <jussi.vesala@reaktor.fi>
2014-09-27 21:22:30 +0300

    simpler express server

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var url = require('url');
+
+var app = express();
+app.set('port', (process.env.PORT || 9000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.get('/api/on', function(request, response) {
+  var query = url.parse(request.url, true).query
+  if (query && query.id) {
+    var res = "OK: turn on id:" + query.id
+    console.log(res)
+    response.send(res)
+  }
+})
+
+app.get('/api/off', function(request, response) {
+  var query = url.parse(request.url, true).query
+  if (query && query.id) {
+    var res = "OK: turn off id:" + query.id
+    console.log(res)
+    response.send(res)
+  }
+})
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
